8b8bc4f4a79698cd873c2ca09009a5c065337413
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-23 15:01:00 +0530

    add and view my contact details to UC2

--- Program.cs
+++ Program.cs
@@ -6,14 +6,29 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            ContactDetails contact = new ContactDetails();
-            contact.firstName = "Mayuri";
-            contact.lastName = "salunkhe";
-            contact.phoneNumber = "9881815815";
-            contact.address = "shahupuri ,satara";
-            contact.email = "mayurisalunkhe01@gmail.com";
-            contact.city = "satara";
-            Console.WriteLine("first name = " + contact.firstName);
+            int num;
+            //guide to user 
+            Console.WriteLine("__Welcome to the address book program__");
+            Console.WriteLine();
+            Console.WriteLine("Select the option.");
+            Console.WriteLine("1- Add contact, 2- View contact");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (num)               //switch case 
+            {
+                case 1:
+                    AddressBook AddObj = new AddressBook();
+                    AddObj.AddContact();
+                    AddObj.View();
+                    break;
+
+                case 2:
+                    AddressBook Addobj2 = new AddressBook();
+                    Addobj2.View();
+                    break;
+
+            }
         }
     }
 }
