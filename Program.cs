750219fbdbacc3eb87dfbfea420f723b20fdcd87
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-23 14:50:26 +0530

    adding my contact details to UC1

--- Program.cs
+++ Program.cs
@@ -15,8 +15,9 @@ namespace AddressBook
             contact.email = "mayurisalunkhe01@gmail.com";
             contact.city = "satara";
             contact.zip = "415002";
-            
-            Console.WriteLine("\n First Name: {0} \n Last Name: {1} \n Address: {2} \n Phone Number: {3}\n Email: {4} \n City: {5} \n State: {6}\n ZipCode: {7}", contact.firstName,contact.lastName, contact.address, contact.phoneNumber, contact.email, contact.city, contact.state, contact.zip);
+
+            Console.WriteLine("\n First Name: {0} \n Last Name: {1} \n Address: {2} \n Phone Number: {3}\n Email: {4} \n City: {5} \n State: {6}\n ZipCode: {7}", contact.firstName, contact.lastName, contact.address, contact.phoneNumber, contact.email, contact.city, contact.state, contact.zip);
         }
     }
 }
+
