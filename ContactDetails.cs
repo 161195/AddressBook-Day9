0c96192a8a0738b8eef4c060c86c48fbdbf55a9a
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-14 23:51:00 +0530

    adding Addressbook to remote

--- ContactDetails.cs
+++ ContactDetails.cs
@@ -4,18 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBook
+namespace AddressBook01
 {
-    class ContactDetails
+    public class Contacts
     {
-        public string firstName { get ; set ;}
+        public string firstName { get; set; }
         public string lastName { get; set; }
         public string address { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
         public string city { get; set; }
         public string state { get; set; }
         public string zip { get; set; }
-        public string phoneNumber { get; set; }
-        public string email { get; set; }
+
 
     }
 }
